f04bf09f85804f3be5eedd7f455f57e545d5fa77
Hemalatha M <2503717610422077@cit.edu.in>
2026-07-09 06:48:09 +0530

    day10:missing number

---
+++ dsa/array/missing number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int i;
+        Arrays.sort(nums);
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]!=i)
+            {
+                return i;
+            }
+        }
+        return n;
+    }
+}
