56934ead5242e18d3bec04c2e32c5adda502cffc
Hemalatha M <2503717610422077@cit.edu.in>
2026-07-13 19:12:30 +0530

    day14:Two sums

---
+++ dsa/array/towsum-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int p=0;
+        int n=numbers.length;
+        int q=n-1;
+        int sum=0;
+        while(p<q)
+        {
+            sum=numbers[p]+numbers[q];
+            if(sum==target)
+            {
+                    return new int[] {p+1,q+1};
+            }
+            else if(sum>target)
+            {
+                q--;
+            }
+            else
+            {
+                p++;
+            }
+        }
+        return new int[] {p+1,q+1};
+        
+    }
+}
