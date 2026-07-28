9a2b656671a2d7399c5dd44410950a9160d326ac
Hemalatha M <2503717610422077@cit.edu.in>
2026-07-28 22:37:40 +0530

    day18:MinimumsizeSubarray

---
+++ dsa/array/MinimumSizeSubarraySum.java
@@ -0,0 +1,28 @@
+java
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n=nums.length;
+        int left=0;
+        int right=0;
+        int sum=0;
+        int i,mini=Integer.MAX_VALUE;
+        for(right=0;right<n;right++)
+        {
+
+            sum+=nums[right];
+
+        while(sum>=target)
+        {
+            sum-=nums[left];
+            mini=Math.min(mini,right-left+1);
+            left++;
+        }
+
+        }
+        if(mini==Integer.MAX_VALUE)
+        {
+            return 0;
+        }
+        return mini;
+    }
+}
