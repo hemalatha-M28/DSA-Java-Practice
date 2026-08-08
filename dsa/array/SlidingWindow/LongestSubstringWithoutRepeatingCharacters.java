ae22d425754aacc5ff752e92ca18dd57e15ba0e5
Hemalatha M <2503717610422077@cit.edu.in>
2026-08-08 16:50:00 +0530

    Update LongestSubstringWithoutRepeatingCharacters.java

--- dsa/array/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
+++ dsa/array/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -9,7 +9,7 @@ class Solution {
         { 
         while(set.contains(s.charAt(i)))
         {
-            set.remove(s.charAt(i));
+            set.remove(s.charAt(left));
             left++;
         }
         set.add(s.charAt(i));
